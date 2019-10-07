db776e3ad48542fd91a4ec4182189e6c59524952
adzunyan <16002823+adzunyan@users.noreply.github.com>
2019-10-07 14:11:04 +0900

    firebaseの設定

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FirebaseService } from '@service/firebase.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    const firebase = new FirebaseService();
+    // firebase.setUser();
+    firebase.getAllData();
+  }
 
 }
---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  db: firebase.firestore.Firestore;
+
+  constructor() {
+    const firebaseConfig = {
+      // apiKey: "AIzaSyC1oyGs9yQ6jHMGYFLkkNcYJqM38Sxp1Fk",
+      // authDomain: "timeline-app-391fe.firebaseapp.com",
+      // databaseURL: "https://timeline-app-391fe.firebaseio.com",
+      projectId: "timeline-app-391fe",
+      // storageBucket: "timeline-app-391fe.appspot.com",
+      // messagingSenderId: "35467543525",
+      // appId: "1:35467543525:web:c9d8ad9aafaa37458b650c"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
+    this.db = firebase.firestore();
+  }
+
+  setUser(): void {
+    let setUser = this.db.collection('user_core').doc("1").set({
+      userId: 1,
+      userName: 'azumi',
+      desc: 'testtest',
+    }).then(() => {
+      console.log("set user");
+    }).catch((error) => {
+      console.log(`error ( ${error} )`);
+    });
+  }
+
+  getAllData(): void {
+    this.db.collection('hoge').get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        console.log(`${doc.id} => ${doc.data()}`);
+      });
+    });
+  }
+
+}
